685bef17c9527ff6596656281fb20913c5305f71
Tino Ehrich <tino@bigpun.me>
2015-11-19 12:16:29 +0100

    typeahead with single and multi result containers

--- typeahead/src/typeahead.element.js
+++ typeahead/src/typeahead.element.js
@@ -1,8 +1,4 @@
 var typeaheadElement = {
-    getHiddenField: function ($that)
-    {
-        return $that.parent().parent().find('input[type=hidden]');
-    },
     getValue: function (obj, attr)
     {
         var keys = attr.split('.');
@@ -30,34 +26,74 @@ var typeaheadElement = {
     {
         return function (e, item)
         {
-            var $hiddenField = typeaheadElement.getHiddenField($(this));
+            var $hiddenField = $(this).parent().parent().find('input[type=hidden]');
 
             if ($hiddenField)
             {
-                var value = typeaheadElement.getValue(item, displayAttr);
                 var id = typeaheadElement.getValue(item, idAttr);
+                var value = typeaheadElement.getValue(item, displayAttr);
+                var label = value;
 
                 if (id)
                 {
                     value = id + delimiter + value;
                 }
 
-                $hiddenField.val(value);
+                var $searchContainer = $(this).parent().parent().parent();
+                var $singleResultContainer = $searchContainer.find('.single-result-container');
+                var $multiResultContainer = $searchContainer.find('.multi-result-container');
+                var template = '<div class="ui fluid selected-item"><a href="#" data-value="{value}"><i class="delete icon"></i></a>{label}</div>';
+
+                if ($singleResultContainer.length)
+                {
+                    $hiddenField.val(value);
+
+                    $singleResultContainer.empty().append(
+                        $(template.replace('{value}', value).replace('{label}', label))
+                    );
+
+                    $searchContainer.addClass('has-single-result');
+                }
+
+                if ($multiResultContainer.length)
+                {
+                    if ($hiddenField.val() !== '')
+                    {
+                        $hiddenField.val($hiddenField.val() + ',');
+                    }
+
+                    $hiddenField.val($hiddenField.val() + value);
+
+                    $multiResultContainer.append(
+                        $(template.replace('{value}', value).replace('{label}', label))
+                    );
+
+                    $searchContainer.addClass('has-multi-result');
+                }
             }
         };
     },
+    removeResult: function (e)
+    {
+        e.preventDefault();
+
+        var $that = $(this)
+        var $fieldContainer = $(this).parent().parent().parent();
+        var $hiddenField = $fieldContainer.find('input[type=hidden]');
+
+        // remove value from hidden input
+        $hiddenField.val($hiddenField.val().replace(new RegExp(',*' + $that.data('value')), ''));
+        $hiddenField.val($hiddenField.val().replace(/^,/, ''));
+
+        // handle indication for single-result-container
+        $fieldContainer.removeClass('has-single-result');
+        $fieldContainer.find('.single-result-container').empty();
+
+        // remove selected-item container
+        $that.parent().remove();
+    },
     close: function (e)
     {
-        var $that = $(this);
-        var $hiddenField = typeaheadElement.getHiddenField($that);
-
-        if ($that.val() !== '' && $hiddenField.val() === '')
-        {
-            $that.val('');
-        }
-        else if ($that.val() === '' && $hiddenField.val() !== '')
-        {
-            $hiddenField.val('');
-        }
+        $(this).typeahead('val', '');
     }
 };
